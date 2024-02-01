9a7c0f34ec53655438d03cdf75dc8fff87a541d2
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-02-01 16:20:55 +0100

    feat(users): add a admin user on launch if it doesn't exist

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,38 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import { AppModule } from './modules';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import * as bcrypt from 'bcrypt';
+
+import { PrismaService } from './prisma/prisma.service';
+import { AppModule } from './modules';
 
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || '0.0.0.0';
+
+async function createAdminUserIfNotExists() {
+  const prisma = new PrismaService();
+  const admin = await prisma.user.findUnique({
+    where: {
+      email: 'admin',
+    },
+  });
+  if (!admin) {
+    const password = 'admin';
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    await prisma.user.create({
+      data: {
+        email: 'admin',
+        username: 'admin',
+        password: hashedPassword,
+        roles: ['ADMIN', 'USER'],
+      },
+    });
+  }
+  prisma.$disconnect();
+}
 
 async function createDocument(app: NestFastifyApplication) {
   const config = new DocumentBuilder()
@@ -27,6 +54,7 @@ async function main() {
   app.setGlobalPrefix('api');
 
   await createDocument(app);
+  await createAdminUserIfNotExists();
   await app.listen(port, host);
 }
 
