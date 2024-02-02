3b1033adf84b3190cad739b901f40ef45e66ce50
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-02-02 16:02:15 +0100

    feat(admin user): create a random password by default for the admin user

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,21 @@ import {
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import * as bcrypt from 'bcrypt';
+import * as fs from 'fs';
 
 import { PrismaService } from './prisma/prisma.service';
 import { AppModule } from './modules';
+import { Logger } from '@nestjs/common';
 
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || '0.0.0.0';
 
 const adminUsername = process.env.ADMIN_USERNAME || 'admin';
-const adminPassword = process.env.ADMIN_PASSWORD || 'admin';
 const adminEmail = process.env.ADMIN_EMAIL || 'admin';
+const adminPasswordSaveFilePath = process.env.ADMIN_PASSWORD_SAVE_FILE_PATH || './admin-password.txt';
 
 async function createAdminUserIfNotExists() {
+  const logger = new Logger('AdminUserCreation');
   const prisma = new PrismaService();
   const admin = await prisma.user.findUnique({
     where: {
@@ -25,7 +28,7 @@ async function createAdminUserIfNotExists() {
     },
   });
   if (!admin) {
-    const password = adminPassword;
+    const password = Math.random().toString(36);
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     await prisma.user.create({
@@ -36,6 +39,9 @@ async function createAdminUserIfNotExists() {
         roles: ['ADMIN'],
       },
     });
+    fs.writeFileSync(adminPasswordSaveFilePath, password);
+    logger.log(`Admin user created, password saved to ${adminPasswordSaveFilePath}`);
+    logger.verbose(`I advise you to change the password as soon as possible and to delete the file ${adminPasswordSaveFilePath}`);
   }
   prisma.$disconnect();
 }
@@ -55,8 +61,8 @@ async function main() {
     AppModule,
     new FastifyAdapter()
   );
-  app.setGlobalPrefix('api');
 
+  app.setGlobalPrefix('api');
   await createDocument(app);
   await createAdminUserIfNotExists();
   await app.listen(port, host);
