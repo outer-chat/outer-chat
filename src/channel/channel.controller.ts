f5222337287acd9757220fcc2c10d1c053c205c8
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-03-28 23:56:49 +0100

    feat(channels): first channels glimpse of implementation

--- src/channel/channel.controller.ts
+++ src/channel/channel.controller.ts
@@ -1,10 +1,13 @@
 import * as swagger from '@nestjs/swagger';
-import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, UseGuards } from '@nestjs/common';
 import { Channel } from './dto/Channel';
 import { ChannelService } from './channel.service';
+import { Roles } from 'src/guards/roles/roles.decorator';
+import { Role } from 'src/guards/roles/roles.enum';
+import { RolesGuard } from 'src/guards/roles/roles.guard';
 
-@swagger.ApiTags('channel')
-@Controller('channel')
+@swagger.ApiTags('channels')
+@Controller('channels')
 export class ChannelController {
     constructor(private readonly channelService: ChannelService) {}
 
@@ -13,8 +16,34 @@ export class ChannelController {
         description: 'Returns the prompted channel',
         type: Channel,
     })
+    @Roles(Role.USER, Role.ADMIN)
+    @UseGuards(RolesGuard)
     @Get(':id')
     getChannel(@Param('id') id: string): Promise<Channel> {
         return this.channelService.getChannel(id);
+    }
+
+    @swagger.ApiOkResponse({
+        status: 200,
+        description: 'Returns all channels, can only be accessed by an admin',
+        type: [Channel],
+    })
+    @Roles(Role.ADMIN)
+    @UseGuards(RolesGuard)
+    @Get()
+    getChannels(): Promise<Channel[]> {
+        return this.channelService.getChannels();
+    }
+
+    @swagger.ApiCreatedResponse({
+        status: 201,
+        description: 'Creates a new channel',
+        type: String,
+    })
+    @Roles(Role.USER, Role.ADMIN)
+    @UseGuards(RolesGuard)
+    @Post()
+    createChannel(@Body() channel: Channel): Promise<string> {
+        return this.channelService.createChannel(channel);
     }
 }
--- src/channel/channel.service.ts
+++ src/channel/channel.service.ts
@@ -13,11 +13,10 @@ export class ChannelService {
       where: {
         id: id,
       },
-      select: {
-        id: true,
-        name: true,
-        createdAt: true,
-        updatedAt: true,
+      include: {
+        recipients: true,
+        messages: true,
+        permissionOverwrites: true,
       },
     });
 
@@ -25,5 +24,64 @@ export class ChannelService {
       throw new BadRequestException(`Channel with id ${id} does not exist!`);
 
     return channel as unknown as Channel;
+  }
+
+  async getChannels() : Promise<Channel[]> {
+    const channels = await this.prisma.channel.findMany({
+      include: {
+        recipients: true,
+        messages: true,
+        permissionOverwrites: true,
+      },
+    });
+
+    channels.forEach((channel) => {
+      channel.recipients.forEach((recipient) => {
+        delete recipient.password;
+        delete recipient.channelId;
+      });
+    });
+
+    return channels as unknown as Channel[];
+  }
+
+  async createChannel(channel: Channel) : Promise<string> {
+
+    if (!channel.recipients || channel.recipients.length === 0)
+      throw new BadRequestException('No recipients have been provided can\'t proceed with channel creation!');
+
+    const newChannel = await this.prisma.channel.create({
+      data: {
+        ...channel,
+        messages: {
+          create: channel.messages?.map((message) => ({
+            ...message,
+            author: {
+              connect: {
+                id: message.authorId,
+              },
+            },
+          })) || [],
+        },
+        recipients: {
+          connect: channel.recipients?.map((recipient) => ({
+            id: recipient.toString(),
+          })) || [],
+        },
+        permissionOverwrites: {
+          create: channel.permissionOverwrites?.map((permissionOverwrite) => ({
+            ...permissionOverwrite,
+            type: Number(permissionOverwrite.type),
+            allow: permissionOverwrite.allow.toString(),
+            deny: permissionOverwrite.deny.toString(),
+          })) || [],
+        },
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    return `Channel with id ${newChannel.id} has been created!`;
   }
 }
