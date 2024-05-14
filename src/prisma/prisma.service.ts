92c87cbaf4d93740993980318798874c4e9830da
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-05-14 16:51:40 +0200

    refactor(mongodb/prisma): pass the env in the service and proper mongodb docker

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,6 +3,16 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+    constructor() {
+        super({
+            datasources: {
+                db: {
+                    url: process.env.DATABASE_URI,
+                },
+            },
+        });
+    }
+
     async onModuleInit() {
         await this.$connect();
     }
